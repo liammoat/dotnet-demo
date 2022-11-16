16a0f9935519c48f5bc5e4baef62b35b7ef02a06
Liam Moat <limoat@microsoft.com>
2022-11-16 07:46:19 +0000

    feat(console): initial console application

---
+++ src/DotNetDemo.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿if (args.Length <= 0)
+{
+    Console.WriteLine("Expected argument <name>");
+    return;
+}
+
+string name = args[0];
+string greeting = GetGreeting(name, "Good afternoon");
+
+Console.WriteLine(greeting);
---
+++ src/DotNetDemo.Core/GreetingService.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetDemo.Core;
+
+public static class GreetingService
+{
+    public static string GetGreeting(string name, string greeting = "Hello")
+    {
+        return $"{greeting}, {name}";
+    }
+}
